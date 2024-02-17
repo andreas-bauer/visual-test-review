ba4ebf26f3f78b5f051b0270b9cec2cf6e30372f
Andreas Bauer <dev@andreasbauer.org>
2024-02-17 16:54:04 +0100

    Add mockup window to home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,9 @@
-import Navbar from './compontens/navbar'
-
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-2'>
-      <div>Content</div>
-      <div>Bottom</div>
+      <div className='mockup-window border w-1/2 bg-base-300'>
+        <div className='flex justify-center px-4 py-16 bg-base-200'>Hello!</div>
+      </div>
     </main>
   )
 }
