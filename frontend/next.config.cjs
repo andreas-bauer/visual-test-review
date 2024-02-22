795b79776a98b87105ab5558f4649d4d0a72e802
Sanyam Jain <sanyamjain3003@gmail.com>
2024-02-22 18:27:24 +0530

    Added version in frontend

--- frontend/next.config.cjs
+++ frontend/next.config.cjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const packageInfo = require('./package.json')
+
 const nextConfig = {
   output: 'export',
   basePath: '/visual-test-review',
+  publicRuntimeConfig: {
+    version: packageInfo.version
+  }
 }
 
 export default nextConfig
