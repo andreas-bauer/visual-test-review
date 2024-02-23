f68d7db0fcc0b5b87a375b6c6b71c51dea3ced1b
Andreas Bauer <dev@andreasbauer.org>
2024-02-23 11:57:42 +0100

    Refactor next.config.mjs to use basePath from env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,11 @@
 import packageInfo from './package.json' assert { type: 'json' }
 
+const nextBasePath = process.env.NEXT_BASE_PATH || ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/visual-test-review',
+  basePath: nextBasePath,
   publicRuntimeConfig: {
     version: packageInfo.version,
   },
